642bc8ece5a7b7db7ab8938d089d62c16e166124
Slavi Dimitrov <slavi@orbisds.com>
2024-07-16 18:55:59 +0300

    Initialise Quiz component and add it to the DOM

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,7 +1,15 @@
 import Header from "./components/Header";
+import Quiz from "./components/Quiz";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </>
+  );
 }
 
 export default App;
